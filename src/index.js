b53263f313c3983140c9ce5b4bfc10d75bc973af
Yamada Keitarou <ke3taro2037@gmail.com>
2026-09-23 00:41:54 +0900

    ページをホスト直下に移動する

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename="/mapartcraft">
+    <Router>
       <Switch>
         <Route path="/:countryCode?/faq" component={FAQ} />
         <Route path="/:countryCode?" component={Root} />
